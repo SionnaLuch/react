db8e5ddcf30c292419b569eb9e360d28087ff654
Sionna Poleszuk <sionna.pol@gmail.com>
2022-01-05 17:55:09 +0100

    aplication finished 15.5

---
+++ src/components/Card/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Card.scss'
+
+class Card extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+  }
+
+  render() {
+    return (
+      <section className={styles.component}>
+        <h2 className={styles.title}>{this.props.title}</h2>
+      </section>
+    );
+  }
+}
+
+export default Card;
