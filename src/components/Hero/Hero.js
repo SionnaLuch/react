8055db63384df5c062ee2f11828bf76facc8317f
Sionna Poleszuk <sionna.pol@gmail.com>
2022-01-05 14:12:58 +0100

    image props 15.4

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './Hero.scss';
-
-const Hero = () => (
+import PropTypes from 'prop-types';
+const Hero = props => (
   <header className={styles.component}>
-    <h2 className={styles.image}>Things to do!</h2>
-    <img className={styles.image} src="https://uploads.kodilla.com/bootcamp/fer/11.react/task-03-2.png"></img>
+    <h2 className={styles.title}>{props.titleText}</h2>
+    <img className={styles.image} src={props.imageHero}></img>
   </header>
 );
-
+Hero.propTypes = {
+    titleText: PropTypes.node.isRequired,
+    imageHero: PropTypes.string.isRequired,
+}
 export default Hero;
